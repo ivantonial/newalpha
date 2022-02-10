8055b947bcd600ac90c7a41dc90b1470d74fc675
Ivan Tonial <ivantonial@gmail.com>
2022-02-10 13:31:52 -0300

    CEP map

--- Pages/04-server/05-zipcode/script.js
+++ Pages/04-server/05-zipcode/script.js
@@ -9,22 +9,32 @@ function getZipCode() {
         .done(responseTop => {
             $('thead').html('');
             $('tbody').html('');
-            $('thead').html(`<tr><th>Endereço</th><th>Bairro</th><th>Cidade</th><th>Estado</th><th>CEP</th></tr>`);
-            $('tbody').html(`<tr><td>${responseTop.address}</td><td>${responseTop.district}</td><td>${responseTop.city}</td><td>${responseTop.state}</td><td>${responseTop.cep}</td></tr>`);
             $('#map').html('');
-            $('#map').html(`<iframe
-            width="600"
-            height="450"
-            style="border:0"
-            loading="lazy"
-            allowfullscreen
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDTqxnTg8xktF1jAigt3byQrPP77g-jiTU&q=${responseTop.lat}%2C${responseTop.lng}">
-        </iframe>`);
+            if (zipCode != "") {
+                $('thead').html(`<tr><th>Endereço</th><th>Bairro</th><th>Cidade</th><th>Estado</th><th>CEP</th></tr>`);
+                $('tbody').html(`<tr><td>${responseTop.address}</td><td>${responseTop.district}</td><td>${responseTop.city}</td><td>${responseTop.state}</td><td>${responseTop.cep}</td></tr>`);
+                $('#map').html(`<iframe
+                    width="600"
+                    height="450"
+                    style="border:0"
+                    loading="lazy"
+                    allowfullscreen
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDTqxnTg8xktF1jAigt3byQrPP77g-jiTU&q=${responseTop.lat}%2C${responseTop.lng}">
+                </iframe>`);
+                $('table').show();
+                $('#map').show();
+            }
+            else {
+                $('table').hide();
+                $('#map').hide();
+            }
         }).fail(response => {
+            $('table').show();
             $('thead').html('');
             $('tbody').html('');
             $('thead').html(`<tr><th>Mensagem</th></tr>`);
             $('#map').html('');
+            $('#map').hide();
             if (response.status === 400) {
                 $('tbody').html(`<tr><td>O Cep inserido (${zipCode}) é inválido!</td></tr>`);
             }
@@ -34,5 +44,6 @@ function getZipCode() {
         });
 }
 
-
+$('table').hide();
+$('#map').hide();
 
